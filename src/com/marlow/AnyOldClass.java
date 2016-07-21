85b8bd1e9117f6f377a74c3fc411b360656d49c6
Marlow14 <c_a_marlow14@yahoo.com>
2016-07-21 10:26:14 -0400

    initial commit

---
+++ src/com/marlow/AnyOldClass.java
@@ -0,0 +1,12 @@
+package com.marlow;
+
+public class AnyOldClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//% use of moduluos and single | and &
+
+	}
+
+}
